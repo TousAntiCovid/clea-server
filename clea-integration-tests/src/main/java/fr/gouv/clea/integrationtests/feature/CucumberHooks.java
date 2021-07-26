9b8b5b96c1c59af5a7c4dbb031000e0e0d779e7b
Bergamote Orange <13178-x-BOrang@users.noreply.gitlab.inria.fr>
2021-07-26 17:35:48 +0200

    before test emptying base

--- clea-integration-tests/src/main/java/fr/gouv/clea/integrationtests/feature/CucumberHooks.java
+++ clea-integration-tests/src/main/java/fr/gouv/clea/integrationtests/feature/CucumberHooks.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.IOException;
 
@@ -13,8 +14,15 @@ public class CucumberHooks {
 
     private final RestHighLevelClient esClient;
 
+    private final JdbcTemplate jdbcTemplate;
+
     @Before
     public void emptyIndices() throws IOException {
         esClient.indices().delete(new DeleteIndexRequest("*"), RequestOptions.DEFAULT);
+    }
+
+    @Before
+    public void emptyTables() {
+        jdbcTemplate.execute("truncate table exposed_visits, cluster_periods, stat_location, stat_reports;");
     }
 }
