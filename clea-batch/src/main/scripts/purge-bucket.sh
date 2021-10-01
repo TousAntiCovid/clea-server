486ce3a4de9c8db53b2bda10940841aa3d7013fa
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2021-10-01 15:22:37 +0200

    refactor: oneshot bucket purge script

--- clea-batch/src/main/scripts/purge-bucket.sh
+++ clea-batch/src/main/scripts/purge-bucket.sh
@@ -1,40 +1,26 @@
 #!/usr/bin/env bash
 
-info() { echo "`date "+%Y-%m-%d %T.999"`  INFO 0 --- [     purge-bucket] purge-bucket.sh                            : $*"; }
+PROFILE=
+BUCKET=
+ENDPOINT=
 
-BUCKET_OUTSCALE=${BUCKET_OUTSCALE:-}
-PROFILE_OUTSCALE=${PROFILE_OUTSCALE:-s3outscale} 
-ENDPOINT_OUTSCALE=${ENDPOINT_OUTSCALE:-}
+BUCKET_FILES_RETENTION_IN_DAYS=15
 
-BUCKET_FILES_RETENTION_IN_DAYS=${BUCKET_FILES_RETENTION_IN_DAYS:-15}
 TODAY_MINUS_RETENTION_DAYS=$(date --date="${BUCKET_FILES_RETENTION_IN_DAYS} days ago" +%Y-%m-%d)
 
-info "Listing bucket files..."
-BUCKET_FILES=${aws --profile=$PROFILE_OUTSCALE --endpoint-url=$ENDPOINT_OUTSCALE s3 ls --recursive s3://${BUCKET_OUTSCALE}/v1}
+echo "Listing bucket files and extracting iterations older than $TODAY_MINUS_RETENTION_DAYS"
+ITERATIONS_TO_REMOVE=$(
+    aws --profile=$PROFILE --endpoint-url=$ENDPOINT s3 ls --recursive s3://${BUCKET}/v1 \
+        | awk -v "date=$TODAY_MINUS_RETENTION_DAYS" '$1 < date {print $4}' \
+        | grep '^v1/[0-9]*/' \
+        | cut -d'/' -f 2 \
+        | sort \
+        | uniq
+)
 
-info "Filtering to keep files older than ${TODAY_MINUS_RETENTION_DAYS}..."
-BUCKET_FILES_TO_REMOVE=${${BUCKET_FILES} | awk -v date=$TODAY_MINUS_RETENTION_DAYS '$1 < date {print $4}'}
+echo "Found $(echo -n "$ITERATIONS_TO_REMOVE" | wc -l) iterations to remove"
 
-info "Extract iterations to remove..."
-BUCKET_ITERATIONS_TO_REMOVE=${BUCKET_FILES_TO_REMOVE | cut -f1 -d"/" | uniq -c}
-
-info "Get first and last iteration to remove..."
-START=${BUCKET_ITERATIONS_TO_REMOVE[0]}
-END=${BUCKET_ITERATIONS_TO_REMOVE[0]}
-
-for i in "${BUCKET_ITERATIONS_TO_REMOVE[@]}"
-do
-    if [[ "$i" -gt "$END" ]]; then
-        END="$i"
-    fi
-
-    if [[ "$i" -lt "$START" ]]; then
-        START="$i"
-    fi
-done
-
-info "Purging bucket iterations..."
-for ((i="$START"; i<="$END"; i++))
-do
-    aws s3 rm aws s3 --profile=$PROFILE_OUTSCALE --endpoint-url=$ENDPOINT_OUTSCALE rm s3://${BUCKET_OUTSCALE}/"$i" --recursive
+for i in $ITERATIONS_TO_REMOVE ; do
+    echo "Removing iteration $i"
+    aws s3 --profile=$PROFILE --endpoint-url=$ENDPOINT rm "s3://$BUCKET/v1/$i" --recursive
 done
