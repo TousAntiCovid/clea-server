62a175a133f095b9519ffda008d9e7edc302dc35
Figue Orange <12540-x-FOrang@users.noreply.gitlab.inria.fr>
2021-10-01 10:28:37 +0200

    feat: add purge script (one shot)

---
+++ clea-batch/src/main/scripts/purge-bucket.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+WORKDIR=${CLEA_BATCH_CLUSTER_OUTPUT_PATH:-/tmp/v1}
+BUCKET=${BUCKET:-}
+
+BUCKET_OUTSCALE=${BUCKET_OUTSCALE:-$BUCKET}
+PROFILE_OUTSCALE=${PROFILE_OUTSCALE:-s3outscale} 
+ENDPOINT_OUTSCALE=${ENDPOINT_OUTSCALE:-}
+
+BUCKET_FILES_RETENTION_IN_DAYS=${BUCKET_FILES_RETENTION_IN_DAYS:-15}
+
+TODAY_MINUS_RETENTION_DAYS=$(date --date="${BUCKET_FILES_RETENTION_IN_DAYS} days ago" +%Y-%m-%d)
+
+info "Listing bucket files..."
+BUCKET_FILES=${aws --profile=$PROFILE_OUTSCALE --endpoint-url=$ENDPOINT_OUTSCALE s3 ls --recursive s3://${BUCKET}/v1}
+
+info "Filtering to keep files older than ${TODAY_MINUS_RETENTION_DAYS}..."
+BUCKET_FILES_TO_REMOVE=${${BUCKET_FILES} | awk -v date=$TODAY_MINUS_RETENTION_DAYS '$1 < date {print $4}'}
+
+info "Extract iterations to remove..."
+BUCKET_ITERATIONS_TO_REMOVE=${BUCKET_FILES_TO_REMOVE | cut -f1 -d"/" | uniq -c}
+
+info "Get first and last iteration to remove..."
+START=${BUCKET_ITERATIONS_TO_REMOVE[0]}
+END=${BUCKET_ITERATIONS_TO_REMOVE[0]}
+
+for i in "${BUCKET_ITERATIONS_TO_REMOVE[@]}"
+do
+    if [[ "$i" -gt "$END" ]]; then
+        END="$i"
+    fi
+
+    if [[ "$i" -lt "$START" ]]; then
+        START="$i"
+    fi
+done
+
+info "Purging bucket iterations..."
+for ((i="$START"; i<="$END"; i++))
+do
+    aws s3 rm aws s3 --profile=$PROFILE_OUTSCALE --endpoint-url=$ENDPOINT_OUTSCALE rm s3://${BUCKET}/"$i" --recursive
+done
