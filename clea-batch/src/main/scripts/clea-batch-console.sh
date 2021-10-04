bac5bd711569d5993029581126b5cbeac1d4bd18
Figue Orange <12540-x-FOrang@users.noreply.gitlab.inria.fr>
2021-10-04 08:15:05 +0000

    feat: updated script to remove outscale bucket files x days ago

--- clea-batch/src/main/scripts/clea-batch-console.sh
+++ clea-batch/src/main/scripts/clea-batch-console.sh
@@ -6,18 +6,17 @@ die()  { echo "`date "+%Y-%m-%d %T.999"` ERROR 0 --- [     clea-batch] clea-batc
 info() { echo "`date "+%Y-%m-%d %T.999"`  INFO 0 --- [     clea-batch] clea-batch.sh                            : $*"; }
 
 WORKDIR=${CLEA_BATCH_CLUSTER_OUTPUT_PATH:-/tmp/v1}
-BUCKET=${BUCKET:-}
 
-BUCKET_OUTSCALE=${BUCKET_OUTSCALE:-$BUCKET}
-BUCKET_SCALEWAY=${BUCKET_SCALEWAY:-$BUCKET}
+[ -n "${BUCKET_OUTSCALE}" ] || die "Environment variable BUCKET_OUTSCALE required"
+[ -n "${BUCKET_SCALEWAY}" ] || die "Environment variable BUCKET_SCALEWAY required"
 
-PROFILE_OUTSCALE=${PROFILE_OUTSCALE:-s3outscale} 
+PROFILE_OUTSCALE=${PROFILE_OUTSCALE:-s3outscale}
 PROFILE_SCALEWAY=${PROFILE_SCALEWAY:-s3scaleway}
 
-ENDPOINT_OUTSCALE=${ENDPOINT_OUTSCALE:-} # use https://oos.eu-west-2.outscale.com/ https://oos.cloudgouv-eu-west-1.outscale.com
-ENDPOINT_SCALEWAY=${ENDPOINT_SCALEWAY:-} # https://s3.fr-par.scw.cloud
+[ -n "${ENDPOINT_OUTSCALE}" ] || die "Environment variable ENDPOINT_OUTSCALE required"
+[ -n "${ENDPOINT_SCALEWAY}" ] || die "Environment variable ENDPOINT_OUTSCALE required"
 
-
+BUCKET_FILES_RETENTION_IN_DAYS=${BUCKET_FILES_RETENTION_IN_DAYS:-15}
 
 set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
@@ -25,20 +24,44 @@ set -o nounset   ## set -u : exit the script if you try to use an uninitialised 
 #set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 set +e
 
+copy_files_to_bucket() {
 
-[ -n "${BUCKET_OUTSCALE}" ] || die "Environment variable BUCKET_OUTSCALE required"
+  local PROFILE=$1
+  local BUCKET=$2
+  local ENDPOINT=$3
+
+  info "Copying to $ENDPOINT ...."
+  AWS_OPTS="--profile=$PROFILE --endpoint-url=$ENDPOINT --no-progress"
+
+  # All files except indexCluster.json
+  aws $AWS_OPTS s3 sync --acl public-read --exclude=clusterIndex.json $WORKDIR s3://${BUCKET}/v1 || die "AWS s3 fails to copy cluster files to bucket"
+  # only indexCluster.json at the root of "v1"
+  aws $AWS_OPTS s3 cp   --acl public-read $(find $WORKDIR -type f -name clusterIndex.json) s3://${BUCKET}/v1/ || die "AWS s3 fails to copy clusterIndex file to bucket"
+
+}
+
+purge_old_bucket_iterations() {
+  # purge bucket files older than x days
+  # add --dryrun for testing purpose
+
+  local PROFILE=$1
+  local BUCKET=$2
+  local ENDPOINT=$3
+
+  TODAY_MINUS_RETENTION_DAYS=$(date --date="${BUCKET_FILES_RETENTION_IN_DAYS} days ago" +%Y-%m-%d)
+  info "Purging bucket files older than ${TODAY_MINUS_RETENTION_DAYS}"
+  aws --profile=$PROFILE --endpoint-url=$ENDPOINT s3 ls --recursive s3://${BUCKET}/v1 \
+      | awk -v date=$TODAY_MINUS_RETENTION_DAYS '$1 < date {print $4}' \
+      | xargs -n1 -t -I {} aws s3 --profile=$PROFILE --endpoint-url=$ENDPOINT rm s3://${BUCKET}/{}
+
+}
 
 if ! java -jar clea-batch.jar $@ ; then
     die "Java batch fails"
 fi
 
-
-info "Copying files...."
-
-
 # Test that output folder exists, computing NBFILES fails if folder doesn't exist
 [ -d $WORKDIR ] || die "Working directory $WORKDIR not exists" 
-
 
 # count that there is at least "n" cluster files (to not push empty list)
 MIN_FILES=1
@@ -54,35 +77,16 @@ if [ $NB_INDEX -gt 1 ] ; then
   die "Many clusterIndex.json found ($NB_INDEX), possible partial or failed batch already present"
 fi
 
-# Copy clusterfiles to s3
-# =======================
+copy_files_to_bucket $PROFILE_OUTSCALE $BUCKET_OUTSCALE  $ENDPOINT_OUTSCALE
+copy_files_to_bucket $PROFILE_SCALEWAY $BUCKET_SCALEWAY  $ENDPOINT_SCALEWAY
 
-
-info "Copying to OUTSCALE ...."
-AWS_OPTS="--profile=$PROFILE_OUTSCALE --endpoint-url=$ENDPOINT_OUTSCALE --no-progress"
-BUCKET="$BUCKET_OUTSCALE"
-
-# All files except indexCluster.json
-aws $AWS_OPTS s3 sync --acl public-read --exclude=clusterIndex.json $WORKDIR s3://${BUCKET}/v1 || die "AWS s3 fails to copy cluster files to bucket"
-# only indexCluster.json at the root of "v1"
-aws $AWS_OPTS s3 cp   --acl public-read $(find $WORKDIR -type f -name clusterIndex.json) s3://${BUCKET}/v1/ || die "AWS s3 fails to copy clusterIndex file to bucket"
-
-# COPY TO SCALEWAY (optional)
-# --------------------
-if [ -n "$BUCKET_SCALEWAY" ] &&  [ -n "$PROFILE_SCALEWAY" ]  &&  [ -n "$ENDPOINT_SCALEWAY" ] ; then
-  info "Copying to SCALEWAY ...."
-  AWS_OPTS="--profile=$PROFILE_SCALEWAY --endpoint-url=$ENDPOINT_SCALEWAY --no-progress"
-  BUCKET=$BUCKET_SCALEWAY
-
-  # All files except indexCluster.json
-  aws $AWS_OPTS s3 sync --acl public-read --exclude=clusterIndex.json $WORKDIR s3://${BUCKET}/v1 || die "AWS s3 fails to copy cluster files to bucket"
-
-  # only indexCluster.json at the root of "v1"
-  aws $AWS_OPTS s3 cp --acl public-read $(find $WORKDIR -type f -name clusterIndex.json) s3://${BUCKET}/v1/ || die "AWS s3 fails to copy clusterIndex file to bucket"
-fi
+purge_old_bucket_iterations $PROFILE_OUTSCALE $BUCKET_OUTSCALE $ENDPOINT_OUTSCALE
+purge_old_bucket_iterations $PROFILE_SCALEWAY $BUCKET_SCALEWAY $ENDPOINT_SCALEWAY
 
 # purge batch temporary files
 info "Purging working files"
 rm -rf $WORKDIR
+
 info "End of clea-batch"
+
 
