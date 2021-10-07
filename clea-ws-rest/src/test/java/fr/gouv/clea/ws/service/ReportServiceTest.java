1ad7ddf37e14e9c0259813799bb184b4a8feb358
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2021-10-07 14:21:04 +0000

    test(clea-ws-rest): add statistics tests

--- clea-ws-rest/src/test/java/fr/gouv/clea/ws/service/ReportServiceTest.java
+++ clea-ws-rest/src/test/java/fr/gouv/clea/ws/service/ReportServiceTest.java
@@ -2,6 +2,7 @@ package fr.gouv.clea.ws.service;
 
 import fr.gouv.clea.ws.configuration.CleaWsProperties;
 import fr.gouv.clea.ws.model.DecodedVisit;
+import fr.gouv.clea.ws.model.ReportStat;
 import fr.gouv.clea.ws.service.model.Visit;
 import fr.inria.clea.lsp.CleaEciesEncoder;
 import fr.inria.clea.lsp.LocationSpecificPart;
@@ -12,6 +13,7 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -191,4 +193,149 @@ class ReportServiceTest {
         return new Visit(qrCode, TimeUtils.ntpTimestampFromInstant(scanTime));
     }
 
+    @Nested
+    class StatisticsTest {
+
+        @Captor
+        ArgumentCaptor<ReportStat> reportStat;
+
+        @Test
+        void duplicate_visits_should_increment_rejected_visits_count() {
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now.minus(4, HOURS)), // pass
+                    newVisit("11111111-1111-1111-1111-111111111111", now), // pass
+                    newVisit("22222222-2222-2222-2222-222222222222", now.minus(3, HOURS)), // pass
+                    newVisit("22222222-2222-2222-2222-222222222222", now), // don't pass
+                    newVisit("33333333-3333-3333-3333-333333333333", now), // pass as a closed with uuid 1 at same time
+                    newVisit("33333333-3333-3333-3333-333333333333", now) // don't pass
+            );
+
+            reportService.report(now.minus(5, DAYS), visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 6)
+                    .hasFieldOrPropertyWithValue("rejected", 2)
+                    .hasFieldOrPropertyWithValue("forwards", 4)
+                    .hasFieldOrPropertyWithValue("backwards", 0)
+                    .hasFieldOrPropertyWithValue("close", 1);
+        }
+
+        @Test
+        void backward_visit_should_increments_backward_visits_count() {
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now.minus(1, DAYS))
+            );
+
+            reportService.report(now, visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 1)
+                    .hasFieldOrPropertyWithValue("rejected", 0)
+                    .hasFieldOrPropertyWithValue("forwards", 0)
+                    .hasFieldOrPropertyWithValue("backwards", 1)
+                    .hasFieldOrPropertyWithValue("close", 0);
+        }
+
+        @Test
+        void forward_visit_should_increments_forward_visits_count() {
+            final var pivotDate = now.minus(1, DAYS);
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now),
+                    newVisit("22222222-2222-2222-2222-222222222222", pivotDate)
+            );
+
+            reportService.report(pivotDate, visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 2)
+                    .hasFieldOrPropertyWithValue("rejected", 0)
+                    .hasFieldOrPropertyWithValue("forwards", 2)
+                    .hasFieldOrPropertyWithValue("backwards", 0)
+                    .hasFieldOrPropertyWithValue("close", 0);
+        }
+
+        @Test
+        void outdated_visits_should_increments_rejected_visits_count() {
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now.minus(15, DAYS)), // don't pass
+                    newVisit("22222222-2222-2222-2222-222222222222", now.minus(13, DAYS)), // pass
+                    newVisit("33333333-3333-3333-3333-333333333333", now.minus(2, DAYS)), // pass
+                    newVisit("44444444-4444-4444-4444-444444444444", now) // pass
+            );
+
+            reportService.report(now.minus(20, DAYS), visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 4)
+                    .hasFieldOrPropertyWithValue("rejected", 1)
+                    .hasFieldOrPropertyWithValue("forwards", 3)
+                    .hasFieldOrPropertyWithValue("backwards", 0)
+                    .hasFieldOrPropertyWithValue("close", 0);
+        }
+
+        @Test
+        void future_visits_should_increments_rejected_visits_count() {
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now), // pass
+                    newVisit("22222222-2222-2222-2222-222222222222", now.plus(2, SECONDS)) // don't pass
+            );
+
+            reportService.report(now.minus(20, DAYS), visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 2)
+                    .hasFieldOrPropertyWithValue("rejected", 1)
+                    .hasFieldOrPropertyWithValue("forwards", 1)
+                    .hasFieldOrPropertyWithValue("backwards", 0)
+                    .hasFieldOrPropertyWithValue("close", 0);
+        }
+
+        @Test
+        void close_visits_should_increments_close_visits_count() {
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now.minus(4, DAYS)), // pass
+                    newVisit("22222222-2222-2222-2222-222222222222", now.minus(2, DAYS)), // close
+                    newVisit("33333333-3333-3333-3333-333333333333", now.minus(2, DAYS).minus(29, MINUTES)) // pass
+            );
+
+            reportService.report(now, visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 3)
+                    .hasFieldOrPropertyWithValue("rejected", 0)
+                    .hasFieldOrPropertyWithValue("forwards", 0)
+                    .hasFieldOrPropertyWithValue("backwards", 3)
+                    .hasFieldOrPropertyWithValue("close", 1);
+        }
+
+        @Test
+        void many_close_visits_should_increments_close_visits_count() {
+            // 2, 3 and 4 are close
+            // 6 and 6 are close
+            final var visits = List.of(
+                    newVisit("11111111-1111-1111-1111-111111111111", now.minus(4, DAYS)),
+                    newVisit("22222222-2222-2222-2222-222222222222", now.minus(2, DAYS)),
+                    newVisit("33333333-3333-3333-3333-333333333333", now.minus(2, DAYS).minus(29, MINUTES)),
+                    newVisit("44444444-4444-4444-4444-444444444444", now.minus(2, DAYS).plus(29, MINUTES)),
+                    newVisit("55555555-5555-5555-5555-555555555555", now.minus(1, DAYS)),
+                    newVisit("66666666-6666-6666-6666-666666666666", now.minus(1, DAYS).plus(29, MINUTES))
+            );
+
+            reportService.report(now.minus(20, DAYS), visits);
+
+            verify(producerService).produceStat(reportStat.capture());
+            assertThat(reportStat.getValue())
+                    .hasFieldOrPropertyWithValue("reported", 6)
+                    .hasFieldOrPropertyWithValue("rejected", 0)
+                    .hasFieldOrPropertyWithValue("forwards", 6)
+                    .hasFieldOrPropertyWithValue("backwards", 0)
+                    .hasFieldOrPropertyWithValue("close", 3);
+        }
+    }
 }
