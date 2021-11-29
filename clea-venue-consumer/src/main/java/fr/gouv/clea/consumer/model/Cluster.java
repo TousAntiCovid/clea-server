3ce4e1aec1ebb086fcf66d4f7bfe4562c9ddb1a5
Sapotille Orange <15519-x-SaOrang@users.noreply.gitlab.inria.fr>
2021-11-29 11:30:56 +0100

    feat(clea-venue-consumer) : Add manually created cluster

---
+++ clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/model/Cluster.java
@@ -0,0 +1,13 @@
+package fr.gouv.clea.consumer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cluster {
+
+    private String deeplink;
+
+    private String date;
+}
