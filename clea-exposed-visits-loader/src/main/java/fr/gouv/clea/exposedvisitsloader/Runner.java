0f1dbd9d9885cfd63ed70b1d3509222a1e500310
Bergamote Orange <13178-x-BOrang@users.noreply.gitlab.inria.fr>
2021-11-30 15:53:47 +0100

    MR comments

--- clea-exposed-visits-loader/src/main/java/fr/gouv/clea/exposedvisitsloader/Runner.java
+++ clea-exposed-visits-loader/src/main/java/fr/gouv/clea/exposedvisitsloader/Runner.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -30,43 +29,43 @@ public class Runner implements ApplicationRunner {
 
     public static final int MAX_VISITS_PER_PLACE = 80;
 
+    private final Random random = new Random();
+
     @Override
     public void run(ApplicationArguments args) {
 
-        final Random r = new Random();
+        final var date = of(2021, 1, 1);
 
-        final LocalDate date = of(2021, 1, 1);
-
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+        final var jdbcTemplate = new JdbcTemplate(ds);
 
         log.info("Starting to fill EXPOSED_VISITS...");
 
         for (int l = 0; l < NUMBER_OF_LOCATIONS_TO_GENERATE; l++) {
             UUID locationId = randomUUID();
-            int venueType = r.nextInt(18) + 1; // 1 to 18
-            int venueCategory1 = r.nextInt(4) + 1; // 1 to 4
-            int venueCategory2 = r.nextInt(4) + 1; // 1 to 4
+            var venueType = random.nextInt(18) + 1; // 1 to 18
+            var venueCategory1 = random.nextInt(4) + 1; // 1 to 4
+            var venueCategory2 = random.nextInt(4) + 1; // 1 to 4
 
             List<ExposedVisit> batch = new ArrayList<>();
 
-            long clusterStart = date.toEpochDay() + (r.nextInt(65) * 1000L);
-            int visitsPerPlace = r.nextInt(MAX_VISITS_PER_PLACE);
-            for (int slot = 0; slot <= visitsPerPlace; slot++) {
+            var clusterStart = date.toEpochDay() + (random.nextInt(65) * 1000L);
+            var visitsPerPlace = random.nextInt(MAX_VISITS_PER_PLACE);
+            for (var slot = 0; slot <= visitsPerPlace; slot++) {
 
                 //@formatter:off
-                ExposedVisit v=ExposedVisit.builder()
+                var exposedVisit = ExposedVisit.builder()
                         .locationTemporaryPublicId(locationId)
                         .venueType(venueType)
                         .venueCategory1(venueCategory1)
                         .venueCategory2(venueCategory2)
                         .periodStart(clusterStart)
                         .timeSlot(slot)
-                        .forwardVisits(r.nextInt(2) * r.nextInt(100)) //  50%=0 or 50%= (1 to 10)
-                        .backwardVisits(r.nextInt(2) * r.nextInt(100))
+                        .forwardVisits(random.nextInt(2) * random.nextInt(100)) //  50%=0 or 50%= (1 to 10)
+                        .backwardVisits(random.nextInt(2) * random.nextInt(100))
                         .build();
                 //@formatter:on
 
-                batch.add(v);
+                batch.add(exposedVisit);
             }
             jdbcTemplate.batchUpdate(
                     "insert into EXPOSED_VISITS (LTId, venue_type, venue_category1, venue_category2, period_start, timeslot,backward_Visits, forward_Visits)"
