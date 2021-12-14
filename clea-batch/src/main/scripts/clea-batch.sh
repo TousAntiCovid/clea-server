172805bec1365fb3dc8bf54dd6408191cc5c7196
Bergamote Orange <13178-x-BOrang@users.noreply.gitlab.inria.fr>
2021-12-14 17:48:36 +0100

    refacto: batch script launch simplification using spring default logging file variable

--- clea-batch/src/main/scripts/clea-batch.sh
+++ clea-batch/src/main/scripts/clea-batch.sh
@@ -3,13 +3,11 @@
 _die()  { echo "`date "+%Y-%m-%d %T.999"` ERROR 0 --- [     clea-batch] clea-batch.sh                            : $*" 1>&2 ; exit 1; }
 
 # redirect logs to log file
-CLEA_BATCH_LOG_FILE_PATH=${CLEA_BATCH_LOG_FILE_PATH:-/logs}
-CLEA_BATCH_LOG_FILE_NAME=${CLEA_BATCH_LOG_FILE_NAME:-clea-batch}
-mkdir -p "$CLEA_BATCH_LOG_FILE_PATH" || _die "can't create log dir: $CLEA_BATCH_LOG_FILE_PATH"
-touch "${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.log" || _die "can't create log file: ${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.log"
-touch "${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.error.log" || _die "can't create log file: ${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.error.log"
+CLEA_BATCH_LOG_FILE_PATH=dirname ${LOGGING_FILE_NAME:?missing logging file name}
 
-exec >> "${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.log"
-exec 2>> "${CLEA_BATCH_LOG_FILE_PATH}/${CLEA_BATCH_LOG_FILE_NAME}.error.log"
+mkdir -p "$CLEA_BATCH_LOG_FILE_PATH" || _die "can't create log dir: $CLEA_BATCH_LOG_FILE_PATH"
+touch "$LOGGING_FILE_NAME" || _die "can't create log file: $LOGGING_FILE_NAME"
+
+exec >> "$LOGGING_FILE_NAME"
 
 source clea-batch-console.sh
